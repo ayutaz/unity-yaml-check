9f15a19491bab93facd4e1fd789a156ecfc47252
yousan <ka1357amnbpdr@gmail.com>
2025-04-18 23:48:59 +0900

    fix: テスト用のボタンクラスとPrfabの作成

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Test
+{
+    public class Button : UIBehaviour,IPointerClickHandler
+    {
+        public UnityEvent onClick;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            onClick?.Invoke();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            onClick.RemoveAllListeners();
+        }
+    }
+}
